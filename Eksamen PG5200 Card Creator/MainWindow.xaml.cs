d4b966ee5d6d6e2319bfd97d2b5f76f1ebcb7faf
Tobias <tobryhn@gmail.com>
2020-10-15 00:34:58 +0200

    Initial commit

---
+++ Eksamen PG5200 Card Creator/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace Eksamen_PG5200_Card_Creator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MakeCard_Click(object sender, RoutedEventArgs e)
+        {
+            ManaCard.Text = ManaValue.Text;
+            DamageCard.Text = DamageValue.Text;
+            HealthCard.Text = HealthValue.Text;
+        }
+
+        private void cardClassType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switch (cardClassType.SelectedItem.ToString())
+            {
+                case "System.Windows.Controls.ComboBoxItem: Death Knight":
+                    changeBaseCard("deathKnight");
+                    Console.WriteLine("bruuuh, Darkbringer");
+                    break;
+                case "System.Windows.Controls.ComboBoxItem: Demon Hunter":
+                    changeBaseCard("demonHunter");
+                    break;
+                case "System.Windows.Controls.ComboBoxItem: Druid":
+                    changeBaseCard("druid");
+                    break;
+                case "System.Windows.Controls.ComboBoxItem: Hunter":
+                    changeBaseCard("hunter");
+                    break;
+                case "System.Windows.Controls.ComboBoxItem: Mage":
+                    changeBaseCard("mage");
+                    break;
+                case "System.Windows.Controls.ComboBoxItem: Neutral":
+                    changeBaseCard("neutral");
+                    break;
+                case "System.Windows.Controls.ComboBoxItem: Paladin":
+                    changeBaseCard("paladin");
+                    break;
+                case "System.Windows.Controls.ComboBoxItem: Priest":
+                    changeBaseCard("priest");
+                    break;
+                case "System.Windows.Controls.ComboBoxItem: Rogue":
+                    changeBaseCard("rogue");
+                    break;
+                case "System.Windows.Controls.ComboBoxItem: Shaman":
+                    changeBaseCard("shaman");
+                    break;
+                case "System.Windows.Controls.ComboBoxItem: Warlock":
+                    changeBaseCard("warlock");
+                    break;
+                case "System.Windows.Controls.ComboBoxItem: Warrior":
+                    changeBaseCard("warrior");
+                    break;
+            }
+            Console.WriteLine(cardClassType.SelectedItem.ToString());
+        }
+
+        private void changeBaseCard(String cardClass)
+        {
+            cardDisplay.Source = new BitmapImage(new Uri("Resources/" + cardClass + "BaseCard.png", UriKind.RelativeOrAbsolute)); 
+        }
+    }
+}
---
+++ Eksamen PG5200 Card Creator/ViewModel/MainViewModel.cs
@@ -0,0 +1,18 @@
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Eksamen_PG5200_Card_Creator.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the MainViewModel class.
+        /// </summary>
+        public MainViewModel()
+        {
+            
+        }
+    }
+}
